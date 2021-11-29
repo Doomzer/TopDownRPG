be3215555a60644cea9b9d4827a13b5ecea8c9f5
Tolik <sharapov-tolik-1@mail.ru>
2021-11-29 17:48:24 +0300

    no message

--- Assets/Script/Portal.cs
+++ Assets/Script/Portal.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class Portal : Collidable
 {
     public string[] sceneNames;
+    public string nextSceneName;
 
     protected override void OnCollide(Collider2D coll)
     {
         if (coll.name == "Player")
         {
             GameManager.instance.SaveState();
-            string sceneName = sceneNames[Random.Range(0, sceneNames.Length)];
+            string sceneName = nextSceneName;
+            if (sceneName == "")
+                sceneName = sceneNames[Random.Range(0, sceneNames.Length)];
+
             UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
         }
     }
